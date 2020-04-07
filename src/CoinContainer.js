3ad69e9df5b6b77490625e973b7090dae444d46b
hiramzamorano <ahzamorano@gmail.com>
2020-04-06 20:26:16 -0700

    Initial commit

---
+++ src/CoinContainer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CoinContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CoinContainer;

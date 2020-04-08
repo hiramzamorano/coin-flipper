1028cbc211fe693429bf2fd7d7c51bc7aebba37d
hiramzamorano <ahzamorano@gmail.com>
2020-04-07 22:23:45 -0700

    Adds Coins img

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import { choice } from './helpers'
+import Coin from "./Coin";
 
 class CoinContainer extends Component {
     static defaultProps = {
         coins: [
             { side: 'heads', imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
-            { side: 'tails', imgSrc: "https://tinyurl.com/react-coin-tails-jpg" }
+            { side: 'tails', imgSrc: "https://tinyurl.com/react-coin-tail-jpg" }
         ]
     }
     constructor(props) {
@@ -19,9 +20,17 @@ class CoinContainer extends Component {
         this.handleClick = this.handleClick.bind(this);
     }
     flipCoin() {
-
+        const newCoin = choice(this.props.coins);
+        this.setState(st => {
+            return {
+                currCoin: newCoin,
+                nFlips: st.nFlips + 1,
+                nHeads: st.nHeads + (newCoin.side === "heads" ? 1 : 0),
+                nTails: st.nTails + (newCoin.side === "tails" ? 1 : 0)
+            };
+        });
     }
-    handleCLick(e){
+    handleClick(e){
         this.flipCoin();
     }
 
@@ -30,6 +39,7 @@ class CoinContainer extends Component {
             <div className="CoinContainer">
                 <h2>Let's FLip a Coin!</h2>
                 <button onClick={this.handleClick}>Flip Me!</button>
+                {this.state.currCoin && <Coin info={this.state.currCoin}/>}
                 <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} Heads and {this.state.nTails} Tails</p>
             </div>
         );

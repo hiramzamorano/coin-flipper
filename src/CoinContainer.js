234b9f8297312e938a975e32a6d9090fbc31d34a
hiramzamorano <ahzamorano@gmail.com>
2020-04-07 22:05:56 -0700

    Adds Choise helper

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
+import { choice } from './helpers'
 
 class CoinContainer extends Component {
+    static defaultProps = {
+        coins: [
+            { side: 'heads', imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
+            { side: 'tails', imgSrc: "https://tinyurl.com/react-coin-tails-jpg" }
+        ]
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            currCoin: null,
+            nFlips: 0,
+            nHeads: 0,
+            nTails: 0
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    flipCoin() {
+
+    }
+    handleCLick(e){
+        this.flipCoin();
+    }
+
     render() {
         return (
-            <div>
-                
+            <div className="CoinContainer">
+                <h2>Let's FLip a Coin!</h2>
+                <button onClick={this.handleClick}>Flip Me!</button>
+                <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} Heads and {this.state.nTails} Tails</p>
             </div>
         );
     }
